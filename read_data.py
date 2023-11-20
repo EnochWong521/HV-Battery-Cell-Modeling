69a834e08917967a3caecf1e7a647d01594d1b8e
EnochWong521 <98492178+EnochWong521@users.noreply.github.com>
2023-11-20 18:45:02 -0500

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -9,9 +9,11 @@ class ReadData:
         self.current_range = current_range
         self.time = self.file[time].tolist()
         self.voltage = self.file[voltage].tolist()
-        self.soc = self.file[soc].tolist()
-        self.initial_voltage = []
-        self.final_voltage = []
+        self.all_soc = self.file[soc].tolist()
+        self.volt_one = []
+        self.volt_two = []
+        self.volt_three = []
+        self.v_drop_soc = []
 
     # for testing purposes
     def print_slopes(self):
@@ -37,12 +39,7 @@ class ReadData:
         ax.grid()
         fig.savefig(f"{self.current_range} {y_title} vs. {x_title} plot.png", dpi=600)
 
-    def esr_graph(self):
-        volt_zero = []
-        volt_one = []
-        esr_drop_list = []
-        esr_list = []
-        soc_list = []
+    def find_voltage_drops(self):
         for index, volts in enumerate(self.voltage):
             # calculate slope
             if index != 0 and index != len(self.voltage) - 1:
@@ -51,21 +48,27 @@ class ReadData:
                 slope = delta_y / delta_x
                 # check if the point is an esr voltage drop
                 if slope < -1:
-                    self.initial_voltage.append(self.voltage[index - 1])
-                    self.final_voltage.append(volts)
-                    soc_list.append(index)
-        soc_list.pop(0)
-        soc_list.pop(-1)
+                    self.volt_one.append(self.voltage[index - 1])
+                    self.volt_two.append(volts)
+                    self.v_drop_soc.append(self.all_soc[index])
+        self.v_drop_soc.pop(0)
+        self.v_drop_soc.pop(-1)
+
+    def esr_graph(self):
+        esr_drop_list = []
+        esr_list = []
+        self.find_voltage_drops()
         # compile a list of esr voltage drops
-        for n, voltage in enumerate(self.initial_voltage):
-            if n != 0 and n != len(self.final_voltage) - 1:
-                esr_drop = voltage - self.final_voltage[n]
+        for n, voltage in enumerate(self.volt_one):
+            if n != 0 and n != len(self.volt_two) - 1:
+                esr_drop = voltage - self.volt_two[n]
                 esr_drop_list.append(esr_drop)
         for i, esr_diff in enumerate(esr_drop_list):
             esr = esr_diff / self.current
             esr_list.append(esr)
-        self.graph_data(soc_list, esr_list, "State of Charge", "Equivalent Series Resistance")
+        self.graph_data(self.v_drop_soc, esr_list, "State of Charge", "Equivalent Series Resistance")
 
     # def resistor_one_graph(self):
+
 
 
