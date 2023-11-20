c62d3acea6f4c10a27bd0fc977071923755ffe2f
EnochWong521 <98492178+EnochWong521@users.noreply.github.com>
2023-11-20 17:39:54 -0500

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -3,48 +3,65 @@ import matplotlib.pyplot as plt
 
 
 class ReadData:
-    def __init__(self, file, current_range, x_axis, y_axis, soc):
-        self.file_name = file
-        data_file = pandas.read_csv(self.file_name)
-        self.current = current_range
-        self.x = x_axis
-        self.y = y_axis
-        self.x_axis = data_file[self.x].tolist()
-        self.y_axis = data_file[self.y].tolist()
-        self.soc = data_file[soc].tolist()
-        self.esr_drop_list = []
+    def __init__(self, file, current, current_range, time, voltage, soc):
+        self.file = pandas.read_csv(file)
+        self.current = current
+        self.current_range = current_range
+        self.time = self.file[time].tolist()
+        self.voltage = self.file[voltage].tolist()
+        self.soc = self.file[soc].tolist()
 
-    def graph_data(self):
+    def graph_data(self, x_list, y_list, x_title, y_title):
         fig, ax = plt.subplots()
-        plt.plot(self.x_axis, self.y_axis, color='blue', linestyle='-', linewidth=1)
-        ax.set_xlabel(self.x, fontsize=14)
-        ax.set_ylabel(self.y, fontsize=14)
-        ax.set_title(f"{self.y} vs. {self.x} graph ({self.current})")
+        plt.plot(x_list, y_list, color='blue', linestyle='-', linewidth=1)
+        ax.set_xlabel(x_title, fontsize=14)
+        ax.set_ylabel(y_title, fontsize=14)
+        ax.set_title(f"{y_title} vs. {x_title} graph ({self.current_range})")
         ax.grid()
-        fig.savefig(f"{self.current} {self.y} vs. {self.x} plot.png", dpi=600)
+        fig.savefig(f"{self.current_range} {y_title} vs. {x_title} plot.png", dpi=600)
 
-    def esr_graph(self, current):
+    def esr_graph(self):
         volt_zero = []
         volt_one = []
         esr_drop_list = []
         esr_list = []
         soc_list = []
-        for index, volts in enumerate(self.y_axis):
+        for index, volts in enumerate(self.voltage):
             # calculate slope
-            if index != 0 and index != len(self.y_axis) - 1:
-                delta_y = volts - self.y_axis[index - 1]
-                delta_x = self.x_axis[index] - self.x_axis[index - 1]
-                slope_one = delta_y / delta_x
+            if index != 0 and index != len(self.voltage) - 1:
+                delta_y = volts - self.voltage[index - 1]
+                delta_x = self.time[index] - self.time[index - 1]
+                slope = delta_y / delta_x
                 # check if the point is an esr voltage drop
-                if slope_one < -5:
-                    volt_one.append(self.y_axis[index - 1])
+                if slope < -5:
+                    volt_one.append(self.voltage[index - 1])
                     volt_zero.append(volts)
                     soc_list.append(index)
+        soc_list.pop(0)
+        soc_list.pop(-1)
         # compile a list of esr voltage drops
-        for n, voltage in volt_one:
+        for n, voltage in enumerate(volt_one):
             if n != 0 and n != len(volt_one) - 1:
                 esr_drop = voltage - volt_zero[n]
                 esr_drop_list.append(esr_drop)
         for i, esr_diff in enumerate(esr_drop_list):
-            esr = esr_diff / current
+            esr = esr_diff / self.current
             esr_list.append(esr)
+        print(soc_list)
+        print(esr_list)
+        print(len(soc_list))
+        print(len(esr_list))
+        self.graph_data(soc_list, esr_list, "State of Charge", "ESR")
+
+    def print_slopes(self):
+        slope_list = []
+        for index, volts in enumerate(self.voltage):
+            # calculate slope
+            if index != 0 and index != len(self.voltage) - 1:
+                delta_y = volts - self.voltage[index - 1]
+                delta_x = self.time[index] - self.time[index - 1]
+                slope_one = delta_y / delta_x
+                slope_list.append(slope_one)
+        slope_list.sort()
+        print(slope_list)
+
