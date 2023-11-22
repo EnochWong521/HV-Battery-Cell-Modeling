3040eadc0d230f104d3788a35e008f6c8ef56db0
EnochWong521 <98492178+EnochWong521@users.noreply.github.com>
2023-11-21 22:41:20 -0500

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,5 +1,6 @@
 import pandas
 import matplotlib.pyplot as plt
+import math
 
 
 class ReadData:
@@ -14,6 +15,8 @@ class ReadData:
         self.volt_two = []
         self.volt_three = []
         self.v_drop_soc = []
+        self.esr = []
+        self.resistance_two = []
 
     # for testing purposes
     def print_slopes(self):
@@ -54,45 +57,57 @@ class ReadData:
                     self.volt_one.append(self.voltage[index - 1])
                     self.volt_two.append(volts)
                     self.v_drop_soc.append(self.all_soc[index])
+                # check if the point is a voltage drop due to resistor one
                 elif slope <= 0:
                     delta_y_two = self.voltage[index + 1] - volts
                     delta_x_two = self.time[index + 1] - self.time[index]
                     slope_two = delta_y_two / delta_x_two
                     if slope_two > 1:
                         self.volt_three.append(self.voltage[index])
-        # eliminate skewed values at the beginning and end of data set
+        # eliminate skewed values at the beginning/end of data set
         self.volt_one.pop(0)
         self.volt_one.pop(-1)
         self.volt_two.pop(0)
         self.v_drop_soc.pop(-1)
 
     def esr_graph(self):
-        esr_drops = []
-        esr_list = []
-        self.find_voltage_drops()
-        new_soc = [s for s in self.v_drop_soc if self.v_drop_soc.index(s) != len(self.v_drop_soc) - 1]
-        # compile a list of esr voltage drops
+        new_soc = [soc for soc in self.v_drop_soc if self.v_drop_soc.index(soc) != len(self.v_drop_soc) - 1]
+        # calculate esr using ohm's law, and compile into a list
         for n, voltage in enumerate(self.volt_one):
             esr_drop = voltage - self.volt_two[n]
             esr = esr_drop / self.current
-            esr_list.append(esr)
-        self.graph_data(new_soc, esr_list, "State of Charge", "Equivalent Series Resistance")
+            self.esr.append(esr)
+        self.graph_data(new_soc, self.esr, "State of Charge", "Equivalent Series Resistance")
 
-    def resistor_one_graph(self):
-        r_one_drop_list = []
+    def resistor_two_graph(self):
+        # calculate resistance of resistor 1 using ohm's law, and compile into a list
         for x, volt in enumerate(self.volt_two):
-            r_one_drop = volt - self.volt_three[x]
-            r_one = r_one_drop / self.current
-            r_one_drop_list.append(r_one)
-        self.graph_data(self.v_drop_soc, r_one_drop_list, "State of Charge", "Resistance 1")
-        
-    '''def capacitor_graph(self):'''
-        
+            r_two_drop = volt - self.volt_three[x]
+            r_two = r_two_drop / self.current
+            self.resistance_two.append(r_two)
+        self.graph_data(self.v_drop_soc, self.resistance_two, "State of Charge", "Resistance 2")
 
+    def capacitor_graph(self):
+        index = 0
+        half_lives = []
+        capacitance_list = []
+        while index < len(self.volt_three):
+            index_one = self.volt_two[index]
+            index_two = self.volt_three[index]
+            half_life_index = (index_two - index_one) / 2
+            if half_life_index is float:
+                half_life_index += 0.5
+                half_lives.append(self.time[int(half_life_index)])
+            else:
+                half_lives.append(self.time[int(half_life_index)])
+            index += 1
+        for n, time in enumerate(half_lives):
+            capacitance = time / (self.resistance_two[n] * math.log(2))
+            capacitance_list.append(capacitance)
+        self.graph_data(self.v_drop_soc, capacitance_list, "State of Charge", "Capacitance")
 
-
-
-
-
-
-
+    def produce_calc_graphs(self):
+        self.find_voltage_drops()
+        self.esr_graph()
+        self.resistor_two_graph()
+        self.capacitor_graph()
